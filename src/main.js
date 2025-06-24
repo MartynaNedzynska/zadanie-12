75aa16011c4aa4dd5d391340f747d96f860c7b43
MartynaNedzynska <marti.nedzynska@gmail.com>
2025-06-24 18:26:57 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import '../src/style.css'
 import javascriptLogo from './javascript.svg'
-import viteLogo from '../src/vite.svg'
+import viteLogo from '../public/vite.svg'
 import { setupCounter } from './counter.js'
 
 document.querySelector('#app').innerHTML = `
